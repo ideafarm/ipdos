2814865fa549e917d632790ae62a2f8d49af2843
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2020-05-23 18:27:27 -0700

    b4 making thirdC::s_socketF an image function

--- precious/domains/com/ideafarm/city/library/snip/ipdos.base.hide.third.1.h
+++ precious/domains/com/ideafarm/city/library/snip/ipdos.base.hide.third.1.h
@@ -1,4 +1,7 @@
 
+#define flDOSfINDfILEoRdIR_DOnOTtESTwATER                          0xe00001ff
+#define flDOSfINDfILEoRdIR_null                                    0xe00000ff
+
 // IPDOS (tm) <> IdeaFarm (tm) Piggyback Distributed Operating System
 
 // For build environment info, see file "\ideafarm.work\backed.up.never\txt\readme.first.ipdos.build.environment.txt"
