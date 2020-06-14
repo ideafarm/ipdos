b4d00b4fdd3e195f084eddc16cddc0324516f34f
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2020-06-13 20:28:16 -0700

    OpenSSL: refresh to use current install image, built from OpenSSL source.
    The install image was obtained by using GitHub Desktop to clone the ideafarm fork of openssl and then building and installing by following the instructions contained therein.  Finally, robocopy used to copy the installed folder to workshop/openssl.
    
    Not tested, other than to verify clean compile and link.

---
+++ precious/domains/com/ideafarm/city/workshop/cmd/ifcreset.bat
@@ -0,0 +1,14 @@
+@echo off
+
+d:
+cd\
+
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\SOIL (tm) Text Being Eaten\production.*"        move "\%%d\IdeaFarm (tm)\WoIdeafarm\SOIL (tm) Text Being Eaten\production.*" "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here"
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\SOIL (tm) Text Being Eaten\dut.*"               move "\%%d\IdeaFarm (tm)\WoIdeafarm\SOIL (tm) Text Being Eaten\dut.*"        "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here"
+
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here\production.*" move "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here\production.*" "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here"
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here\dut.*"        move "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here\dut.*"        "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here"
+
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\SOIL (tm) Text Being Eaten\*"                   del  "\%%d\IdeaFarm (tm)\WoIdeafarm\SOIL (tm) Text Being Eaten\*"              /q
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here\*"            del  "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here\*"       /q
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up Rejected Files Here\*"                  del  "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up Rejected Files Here\*"             /q
---
+++ precious/domains/com/ideafarm/city/workshop/cmd/ifcunreset.bat
@@ -0,0 +1,8 @@
+@echo off
+
+d:
+cd\
+
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here\production.*" move "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here\production.*" "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here"
+
+for /d %%d in (ideafarm.home.*) do @if exist "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here\dut.*"        move "\%%d\IdeaFarm (tm)\WoIdeafarm\Drop a Copy of SOIL (tm) Text Here\dut.*"        "\%%d\IdeaFarm (tm)\WoIdeafarm\Pick Up SOIL (tm) Reply Text Here"
