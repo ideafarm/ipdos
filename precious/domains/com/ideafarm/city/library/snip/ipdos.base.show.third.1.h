8e771bfbf11d9fd37460d6ac2eeb98dc67c564db
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2020-07-30 12:48:49 -0700

    header refresh ; update code for TOOLwHITElISTuPDATER

--- precious/domains/com/ideafarm/city/library/snip/ipdos.base.show.third.1.h
+++ precious/domains/com/ideafarm/city/library/snip/ipdos.base.show.third.1.h
@@ -1,4 +1,6 @@
 
+#define ifcIDaDAM_TOOLwHITElISTuPDATER                                 0x51000793
+
 // IPDOS (tm) <> IdeaFarm (tm) Piggyback Distributed Operating System
 
 // For build environment info, see file "\ideafarm.work\backed.up.never\txt\readme.first.ipdos.build.environment.txt"
