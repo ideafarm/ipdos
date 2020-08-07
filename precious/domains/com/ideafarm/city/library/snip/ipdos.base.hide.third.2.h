6ee305b3f33f7a9d29f8e6946f4346f663828fc9
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2020-08-07 16:04:28 -0700

    http: spoof detection
    the code now detects when the local host is being spoofed by a peer.  This is done by comparing idPortPeer with the port that the WATCH adam has registered in homeS when it attempts to request deadman.

--- precious/domains/com/ideafarm/city/library/snip/ipdos.base.hide.third.2.h
+++ precious/domains/com/ideafarm/city/library/snip/ipdos.base.hide.third.2.h
@@ -1,45 +1,3 @@
-//SOURCE: \ideafarm.home.1\precious\domains\com\ideafarm\city\library\dictionary\1snip.11*.* : 1snip.112004d1.setinoutframe BEGIN
-
-
-//
-// Copyright (c) 1992-2020 Wo Of Ideafarm.  All rights reserved.  See IDEAFARM.COM for permitted uses.
-//
-// This proprietary software was crafted at great expense and with great hardship by one man.  It took 28 years.
-// Respecting the rights of other people is an important part of empowering one another.
-//
-
-/*
-*/
-
-/**/
-
-/*1*//*SETiNoUTfRAME*//*1*/
-
-#define SETiNoUTfRAME                                                                                                                           \
-                                                                                                                                                \
-    countT    cgin  = *ppcGlyph[    offd ] ;                                                                                                    \
-    countT*   psin  =  ppcSize[     offd ] ;                                                                                                    \
-    flagsT*   pfin  =  pFlagsTail[  offd ] ;                                                                                                    \
-    mPoint3S* ptin  =  ppPointTail[ offd ] ;                                                                                                    \
-    mPoint3S* ppin  =  ppPoint[     offd ] ;                                                                                                    \
-                                                                                                                                                \
-    if( ++ offd == 3 ) offd = 1 ; /* OFFSET 0 IS ONLY USED WO TIME ; AFTER THAT ONLY OFFSETS 1 AND 2 ARE USED */                                \
-                                                                                                                                                \
-    countT&   cgout = *ppcGlyph[    offd ] ;                                                                                                    \
-    countT*   psout =  ppcSize[     offd ] ;                                                                                                    \
-    flagsT*   pfout =  pFlagsTail[  offd ] ;                                                                                                    \
-    mPoint3S* ptout =  ppPointTail[ offd ] ;                                                                                                    \
-    mPoint3S* ppout =  ppPoint[     offd ] ;
-
-
-//
-// Respecting the rights of other people is an important part of empowering one another.
-// This proprietary software was crafted at great expense and with great hardship by one man.  It took 28 years.
-//
-// Copyright (c) 1992-2020 Wo Of Ideafarm.  All rights reserved.  See IDEAFARM.COM for permitted uses.
-//
-
-//SOURCE: \ideafarm.home.1\precious\domains\com\ideafarm\city\library\dictionary\1snip.11*.* : 1snip.112004d1.setinoutframe END
 //SOURCE: \ideafarm.home.1\precious\domains\com\ideafarm\city\library\dictionary\1snip.11*.* : 1snip.112004d2.flsurfc BEGIN
 
 
@@ -26641,6 +26599,7 @@ code may use setIfZeAM on *(countT*)this
     countT              idPortListServerIdAccountDescription ; //SET AND MAINTAINED BY ifcIDaDAM_LISTsERVERiDaCCOUNTdESCRIPTION
     countT              idPortListMoneyBuckets ; // SET AND MAINTAINED BY ifcIDaDAM_LISTsERVERmONEYbUCKETS
     countT              idPortCloak ; // SET AND MAINTAINED BY ifcIDaDAM_CLOAK
+    countT              idPortWatchHttp ; // SET AND MAINTAINED BY ifcIDaDAM_WATCH
     idPortTimeC         idptRelay ; //SET AND MAINTAINED BY b150104 (U::THIS COMMENT PROBABLY IDENTIFIES THE WRONG ADAM)
     countT              idRank ;
     idPortC             idpTeat ;
