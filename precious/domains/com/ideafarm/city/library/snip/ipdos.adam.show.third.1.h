67c8e656d76dd0ba3db11feb835099a29c2b6ac9
Wo'O Ideafarm <ideafarmcity@gmail.com>
2021-01-06 18:51:26 -0800

    Changing hardcoded "factory1" "factory2" to symbolic constant
    postCRAFTWORKfACTORY

--- precious/domains/com/ideafarm/city/library/snip/ipdos.adam.show.third.1.h
+++ precious/domains/com/ideafarm/city/library/snip/ipdos.adam.show.third.1.h
@@ -1,4 +1,6 @@
 
+#define postCRAFTWORKfACTORY "factory2"
+
 // IPDOS (tm) <> IdeaFarm (tm) Piggyback Distributed Operating System
 
 // For build environment info, see file "\ideafarm.work\backed.up.never\txt\readme.first.ipdos.build.environment.txt"
