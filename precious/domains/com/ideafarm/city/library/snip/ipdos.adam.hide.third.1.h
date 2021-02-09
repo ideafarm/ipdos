775aa550cf9f0b39ebb5915615dfa25905b4cd8b
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2021-02-09 10:08:40 -0800

    header refresh
    Status of relayC is being investigated.  With this commit, bundle contains only bang relay and bang gorilla, and when run as service it sees both its own cioPut and the cioPut's of home 1.  But home 1 sees nothing.

--- precious/domains/com/ideafarm/city/library/snip/ipdos.adam.hide.third.1.h
+++ precious/domains/com/ideafarm/city/library/snip/ipdos.adam.hide.third.1.h
@@ -1,6 +1,4 @@
 
-#define ifcIDaDAM_GORILLAbANG                                          0x510007a6
-
 // IPDOS (tm) <> IdeaFarm (tm) Piggyback Distributed Operating System
 
 // For build environment info, see file "\ideafarm.work\backed.up.never\txt\readme.first.ipdos.build.environment.txt"
@@ -11814,6 +11812,7 @@ if ifcIDtYPEjOBcOURSE_NAMED is used then this value must be followed immediately
 #define ifcIDaDAM_CLOAK                                                0x51000767
 #define ifcIDaDAM_TOOLwHITElISTuPDATER                                 0x51000793
 #define ifcIDaDAM_TOOLiNoUTfRAMEctELEMETRYrEPORTER                     0x510007a1
+#define ifcIDaDAM_GORILLAbANG                                          0x510007a6
 #define ifcIDaDAM_STARTER1                                             0x51001001
 #define ifcIDaDAM_STARTER2                                             0x51001002
 #define ifcIDaDAM_STARTER3                                             0x51001003
