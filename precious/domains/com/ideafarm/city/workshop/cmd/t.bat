db4a3ab08e5b521e605be116251144694a670834
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2021-02-09 14:36:55 -0800

    relayC: more developed dut

--- precious/domains/com/ideafarm/city/workshop/cmd/t.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/t.bat
@@ -3,12 +3,19 @@
 rem call g m
 rem wdw ideafarm.41000002.ipdos-wm 51000620 !idHome 10000008 !debug !noSystemThreads
 
+call zapidea.bat
+call freshhome2.bat
+
 set idHomeG=1
 call g m
 start  ideafarm.41000002.ipdos-wm 51000511 !idHome %idHomeG%0000008
+
+pause
 
 set idHomeG=2
 call g m
 start  ideafarm.41000002.ipdos-wm 51000511 !idHome %idHomeG%0000008
 
 set idHomeG=1
+
+:FIN
