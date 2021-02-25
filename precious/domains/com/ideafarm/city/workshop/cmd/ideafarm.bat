9c4250d7fb9d7e6fbc9512488c3faf77fe95090d
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2021-02-25 14:10:36 -0800

    polishing build experience

--- precious/domains/com/ideafarm/city/workshop/cmd/ideafarm.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/ideafarm.bat
@@ -12,6 +12,56 @@ echo IdeaFarm (tm) drive is: %idrive%
 
 set iworkshop=%idrive%\ideafarm.home.1\precious\domains\com\ideafarm\city\workshop
 
+if not exist \tmp.ideafarm\* md \tmp.ideafarm
+if exist \tmp.ideafarm\vc.installed.txt goto :VCINSTALLED
+
+copy %iworkshop%\visualstudio\vc.installed.txt \tmp.ideafarm
+
+echo .
+echo I am about to install the Visual Studio runtimes.
+echo If they are both (64-bit and 32-bit) are already installed, an error message will be displayed.
+echo Click the "Install" and then the "Close" button for each of these two programs.
+echo .
+pause
+
+%iworkshop%\visualstudio\VC_redist.x64.exe
+%iworkshop%\visualstudio\VC_redist.x86.exe
+
+
+:VCINSTALLED
+
+if exist a:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive a.
+if exist b:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive b.
+if exist c:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive c.
+if exist d:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive d.
+if exist e:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive e.
+if exist f:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive f.
+if exist g:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive g.
+if exist h:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive h.
+if exist i:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive i.
+if exist j:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive j.
+if exist k:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive k.
+if exist l:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive l.
+if exist m:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive m.
+if exist n:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive n.
+if exist o:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive o.
+if exist p:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive p.
+if exist q:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive q.
+if exist r:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive r.
+if exist s:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive s.
+if exist t:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive t.
+if exist u:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive u.
+if exist v:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive v.
+if exist w:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive w.
+if exist x:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive x.
+if exist y:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive y.
+if exist z:\ideafarm.home.* echo WARNING: ideafarm.home.* exists on drive z.
+echo .
+echo All ideafarm.home.* folders must be in the root folder of the same local drive.
+echo If execution fails, this is the first thing to check for.
+echo .
+pause
+
 rem COMMENT OUT THESE LINES IF YOU HAVE INSTALLED THE WiX Toolset:
 set path=%path%;%iworkshop%\wix\WiX Toolset v3.11\bin
 set wix=%iworkshop%\wix\WiX Toolset v3.11\
