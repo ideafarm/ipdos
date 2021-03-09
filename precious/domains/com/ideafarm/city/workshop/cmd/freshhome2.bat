9bbf7dd4a4497ee43053761f01eac38939f28283
Wo'O Ideafarm <ideafarm@users.noreply.github.com>
2021-03-09 11:24:18 -0800

    remove dictionary.bad ; develop freshhome2.bat

--- precious/domains/com/ideafarm/city/workshop/cmd/freshhome2.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/freshhome2.bat
@@ -2,23 +2,33 @@
 
 rem I FRESHEN HOME 2 (SO THAT IT CAN BE USED FOR MULTIHOME TESTING)
 
+if not exist %idrive%\ideafarm.home.1\*                                                          goto :FIN
 if not exist %idrive%\ideafarm.home.1\ephemeral\domains\com\ideafarm\city\park\exedll\1\master\* goto :FIN
-if not exist %idrive%\ideafarm.home.1\ephemeral\domains\com\ideafarm\city\park\exedll\1\hover\* goto :FIN
-if not exist %idrive%\ideafarm.home.2\ephemeral\domains\com\ideafarm\city\park\exedll\1\master\* goto :FIN
-if not exist %idrive%\ideafarm.home.2\ephemeral\domains\com\ideafarm\city\park\exedll\1\hover\* goto :FIN
+if not exist %idrive%\ideafarm.home.1\ephemeral\domains\com\ideafarm\city\park\exedll\1\hover\*  goto :FIN
+
+if     exist %idrive%\ideafarm.home.2\* goto :FRESH2
+
+robocopy %idrive%\ideafarm.home.1 %idrive%\ideafarm.home.2 /s
+goto :FIN
+
+
+:FRESH2
 
 %idrive%
 
+if not exist %idrive%\ideafarm.home.2\ephemeral\domains\com\ideafarm\city\park\exedll\1\master\* goto :FIN
+if not exist %idrive%\ideafarm.home.2\ephemeral\domains\com\ideafarm\city\park\exedll\1\hover\*  goto :FIN
+
 cd %idrive%\ideafarm.home.2\ephemeral\domains\com\ideafarm\city\park\exedll\1\master
-del */q
+if exist * del */q
 copy %idrive%\ideafarm.home.1\ephemeral\domains\com\ideafarm\city\park\exedll\1\master\*
 
 cd %idrive%\ideafarm.home.2\ephemeral\domains\com\ideafarm\city\park\exedll\1\hover
-del */q
+if exist * del */q
 copy %idrive%\ideafarm.home.1\ephemeral\domains\com\ideafarm\city\park\exedll\1\hover\*
 
 cd %idrive%\ideafarm.home.2\ephemeral\domains\com\ideafarm\city\workshop\2source
-del */q
+if exist * del */q
 copy %idrive%\ideafarm.home.1\ephemeral\domains\com\ideafarm\city\workshop\2source\*
 
 :FIN
